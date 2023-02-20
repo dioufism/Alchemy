bf918cbd40f1b1b6ceda7cbb0b952da8308d9871
Ousmane <dioufism@gmail.com>
2023-02-20 00:10:36 -0600

    Initial Commit

---
+++ Alchemy/Alchemy.h
@@ -0,0 +1,18 @@
+//
+//  Alchemy.h
+//  Alchemy
+//
+//  Created by ousmane diouf on 2/20/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Alchemy.
+FOUNDATION_EXPORT double AlchemyVersionNumber;
+
+//! Project version string for Alchemy.
+FOUNDATION_EXPORT const unsigned char AlchemyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alchemy/PublicHeader.h>
+
+
